8767fdb709bfd21c48d702716d2c658f385ee870
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-06-21 17:49:05 +0300

    02_21.06

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const menuBtn = document.querySelector(".menu__btn");
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".menu__list");
 
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("menu--open");
